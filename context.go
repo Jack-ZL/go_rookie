d6f4c242fcb927378ff705730b385d3d2c2d4873
Jack_Z <jackz@admin.com>
2022-09-10 00:21:03 +0800

    HTML渲染

--- context.go
+++ context.go
@@ -6,3 +6,11 @@ type Context struct {
 	W http.ResponseWriter
 	R *http.Request
 }
+
+func (c *Context) HTML(status int, html string) error {
+	// 状态值200（即使不设置默认也是200）
+	c.W.Header().Set("Content-Type", "text/html; charset=utf-8")
+	c.W.WriteHeader(status)
+	_, err := c.W.Write([]byte(html))
+	return err
+}
