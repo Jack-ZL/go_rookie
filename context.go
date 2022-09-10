5144f28adcbd2543e7209c12e515eb888e723cab
Jack_Z <jackz@admin.com>
2022-09-11 01:24:22 +0800

    form表单文件处理

--- context.go
+++ context.go
@@ -4,13 +4,16 @@ import (
 	"errors"
 	"github.com/Jack-ZL/go_rookie/render"
 	"html/template"
+	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 )
 
-const defaultMaxMemory = 32 << 20 // 默认是分配32M的内存
+const defaultMultipartMemory = 32 << 20 // 默认是分配32M的内存
 
 type Context struct {
 	W          http.ResponseWriter
@@ -158,7 +161,7 @@ func (c *Context) get(cache map[string][]string, key string) (map[string]string,
  */
 func (c *Context) initPostFormCache() {
 	if c.R != nil {
-		if err := c.R.ParseMultipartForm(defaultMaxMemory); err != nil {
+		if err := c.R.ParseMultipartForm(defaultMultipartMemory); err != nil {
 			if !errors.Is(err, http.ErrNotMultipart) {
 				log.Println(err)
 			}
@@ -238,6 +241,78 @@ func (c *Context) GetPostFormMap(key string) (map[string]string, bool) {
 func (c *Context) PostFormMap(key string) map[string]string {
 	dicts, _ := c.GetPostFormMap(key)
 	return dicts
+}
+
+/**
+ * FormFile
+ * @Author：Jack-Z
+ * @Description: form文件获取（单个）
+ * @receiver c
+ * @param name
+ * @return *multipart.FileHeader
+ */
+func (c *Context) FormFile(name string) *multipart.FileHeader {
+	file, header, err := c.R.FormFile(name)
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+	return header
+}
+
+/**
+ * FormFiles
+ * @Author：Jack-Z
+ * @Description: form文件获取（同一参数名中包含多个文件的情况）
+ * @receiver c
+ * @param name
+ * @return []*multipart.FileHeader
+ */
+func (c *Context) FormFiles(name string) []*multipart.FileHeader {
+	multipartForm, err := c.MultipartForm()
+	if err != nil {
+		return make([]*multipart.FileHeader, 0)
+	}
+	return multipartForm.File[name]
+}
+
+/**
+ * SaveUploadedFile
+ * @Author：Jack-Z
+ * @Description: 将上传的文件保存
+ * @receiver c
+ * @param file
+ * @param dst
+ * @return error
+ */
+func (c *Context) SaveUploadedFile(file *multipart.FileHeader, dst string) error {
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, src)
+	return err
+}
+
+/**
+ * MultipartForm
+ * @Author：Jack-Z
+ * @Description: 获取整体的form表单内容（包含普通的key-value和文件类型）
+ * @receiver c
+ * @return *multipart.Form
+ * @return error
+ */
+func (c *Context) MultipartForm() (*multipart.Form, error) {
+	err := c.R.ParseMultipartForm(defaultMultipartMemory)
+	return c.R.MultipartForm, err
 }
 
 /**
