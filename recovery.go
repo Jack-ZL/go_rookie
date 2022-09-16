1f2aec33bb40912b8d565e0f6dc61aa286850b17
Jack_Z <jackz@admin.com>
2022-09-16 23:21:34 +0800

    输出错误所在的文件和行号

--- recovery.go
+++ recovery.go
@@ -1,12 +1,38 @@
 package go_rookie
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+	"strings"
+)
+
+/**
+ * detailMsg
+ * @Author：Jack-Z
+ * @Description: 输出错误所在的文件和行号
+ * @param err
+ * @return string
+ */
+func detailMsg(err any) string {
+	var pcs [32]uintptr
+	n := runtime.Callers(3, pcs[:])
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("%v\n", err))
+	for _, pc := range pcs[0:n] {
+		fn := runtime.FuncForPC(pc)
+		file, line := fn.FileLine(pc)
+		sb.WriteString(fmt.Sprintf("\n\t%s:%d", file, line))
+	}
+
+	return sb.String()
+}
 
 func Recovery(next HandlerFunc) HandlerFunc {
 	return func(ctx *Context) {
 		defer func() {
 			if err := recover(); err != nil {
-				ctx.Logger.Error(err)
+				ctx.Logger.Error(detailMsg(err))
 				ctx.Fail(http.StatusInternalServerError, "Internal Server Error")
 			}
 		}()
