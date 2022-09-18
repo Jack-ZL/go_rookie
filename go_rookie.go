f0c79d4777987545bec2063931c6bd957b3f8087
Jack_Z <jackz@admin.com>
2022-09-18 15:15:42 +0800

    支持https安全

--- go_rookie.go
+++ go_rookie.go
@@ -273,6 +273,22 @@ func (e *Engine) Run() {
 	}
 }
 
+/**
+ * RunTLS
+ * @Author：Jack-Z
+ * @Description: 支持https安全
+ * @receiver e
+ * @param addr
+ * @param certFile
+ * @param keyFile
+ */
+func (e *Engine) RunTLS(addr, certFile, keyFile string) {
+	err := http.ListenAndServeTLS(addr, certFile, keyFile, e.Handler())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func (e *Engine) Use(middles ...MiddlewareFunc) {
 	e.middles = middles
 }
@@ -286,4 +302,8 @@ func (e *Engine) Use(middles ...MiddlewareFunc) {
  */
 func (e *Engine) RegisterErrorHandler(handler ErrorHandler) {
 	e.errorHandler = handler
+}
+
+func (e *Engine) Handler() http.Handler {
+	return e
 }
